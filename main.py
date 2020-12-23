c1ff3e1d79bfa9c64919aa1dbf4db1b593b92697
Mahmoud Mahmoud <1906425@brunel.ac.uk>
2020-12-23 21:03:16 +0000

    I have started adding in the inventory system for the player, which shows the name of the item and how much of that specifc item they have

--- main.py
+++ main.py
@@ -2,9 +2,18 @@
 ##You are currently sealed off in an alternative dimension,
 ##The dimension is home to alien creature, their weakness is...
 
-#REQUIRED: Inventory, different paths which affect the outcome of the game
+#Write some pseudocode in tandum with the actual code!
+
+#REQUIRED:
+#inventory - done using a function, includes either a dictionary, list, or tuple or a combination of the three#the
+#the inventory function should be called whenever the player finds something new to add, a weapon breaks, or if they
+#want to open up the inventory themselves(OPTIONAL)
+#if player picks up something i is added in the inventory
+#checks for which scene the player is in when picking up an item
 
 import time
+
+inventory = {}
 
 Event = 0
 
@@ -16,17 +25,29 @@ print("..")
 time.sleep(2)
 print("...")
 
+
+
+def PlayerInventory():
+    if Event == 0:
+        inventory["Stick"] = 5
+        print("The stick was added to the inventory")
+        print(inventory)
+
+
 while True:
-    event = 0
-    usr_input = str(input("Press A to wake up"))
-    if usr_input == "A" or usr_input == "a":
-        print("Nghh...")
-        time.sleep(1)
-        print("My head... where I am..?")
-        print(" 'I took in my surroundings while  ")
-        break
+    if Event == 0:
+        usr_input = str(input("Press A to wake up"))
+        if usr_input == "A" or usr_input == "a":
+            print(" 'Nghh...' ")
+            time.sleep(1)
+            print(" 'My head... where I am..?' ")
+            usr_input = input("I took in my surroundings as my consciousness slowly reformed. Right next to me was "
+                              "a stick on the ground" '(pick up? y/n)')
 
-    elif usr_input != "A" or usr_input !="a":
-        continue
+            if usr_input == "y":
+                PlayerInventory()
+            break
 
+        elif usr_input != "A" or usr_input !="a":
+            continue
 
