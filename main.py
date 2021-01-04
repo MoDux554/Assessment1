1968803e01d855163dabd512845228d8540efc3a
Mahmoud Mahmoud <1906425@brunel.ac.uk>
2021-01-04 16:04:44 +0000

    If the player picks up the bunch of sticks at the at the start of the game, they will have the opportunity to light them up for later. They have to enter in a number between 1 and 5 to make their desired number of torches, which also subtracts from the original sticks in the inventory.

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-##IDEAS FOR TEXT BASED ADVENTURE
-##You are currently sealed off in an alternative dimension,
-##The dimension is home to alien creature, their weakness is...
-
-#Write some pseudocode in tandum with the actual code!
-
-#REQUIRED:
-#inventory - done using a function, includes either a dictionary, list, or tuple or a combination of the three#the
-#the inventory function should be called whenever the player finds something new to add, a weapon breaks, or if they
-#want to open up the inventory themselves(OPTIONAL)
-#if player picks up something i is added in the inventory
-#checks for which scene the player is in when picking up an item
-
 import time
 
 inventory = {}
@@ -19,8 +6,9 @@ Yes = ["y", "Y", "yes", "Yes"]
 No = ["n", "N", "no", "No"]
 Confirm = ["a", "A"]
 
-Event = 0
+valueChange = 0
 
+Event = 0
 
 print(".")
 time.sleep(2)
@@ -31,44 +19,64 @@ print("...")
 
 
 
-def PlayerInventory(): #adds things the player picks up during the game into the inventory, these correspond
-                       #to the "events" the player is in
+def PlayerInventory(): #Adds things the player picks up during the game into the inventory, these correspond
+                       #to the "events" the player is in.
     if Event == 0:
-        inventory["Stick"] = 5
-        print("The stick was added to the inventory")
+        inventory["Sticks"] = 5
+        print("The sticks were added to the inventory")
         print(inventory)
 
+    elif Event == 1:
+        # The litstick is added here so that more can be added to it from the user input
+        inventory["LitStick"] = 0
+        global valueChange #this variable is referenced outside of the PlayerInventory function
+        while True:
+            try:
+                valueChange = int(input("How many of your sticks would you like to light up?"))
+                if valueChange <=5 and valueChange >=1:
+                    inventory["Sticks"] -= valueChange
+                    inventory["LitStick"] += valueChange
+                    print(inventory)
+                    break
+                else:
+                    print("Please enter in a number between 1 and 5")
+                    continue
+            except ValueError:
+                print("please enter in a number")
 
-while True:
-    usr_input = str(input("Press A to wake up"))
-    if usr_input in Confirm:
-        print(" 'Nghh...' ")
-        time.sleep(1)
-        print(" 'My head... where I am..?' ")
-        usr_input = input("I took in my surroundings as my consciousness slowly reformed. Right next to me was "
-                          "a stick on the ground" '(pick up? y/n)')
-        if usr_input in Yes:
-            PlayerInventory()
-            Event = 1
-        elif usr_input in No:
-            Event = 1
-    else:
-        continue
+
+
+while True: #This is where all the events of the game play out
+    if Event == 0:
+        usr_input = str(input("Press A to wake up"))
+        if usr_input in Confirm:
+            print(" 'Nghh...' ")
+            time.sleep(1)
+            print(" 'My head... where I am..?' ")
+            usr_input = input("I took in my surroundings as my consciousness slowly reformed. Right next to me were a bunch of sticks "
+                              "on the ground" '(pick them up? y/n)')
+            if usr_input in Yes:
+                PlayerInventory()
+                Event = 1
+            elif usr_input in No:
+                Event = 1
+        else:
+            continue #if the user inputs anything that is not in the yes,no or confirm lists, the first if statement
+                      #wil be repeated
 
 
     if Event == 1:
         print("As you walk along, you notice some fire on the wall")
-        if "Stick" in inventory:
+        if "Sticks" in inventory:#checks if the sticks are in the player's inventory system
             usr_input = input("do you want to use your stick to make a torch?")
             if usr_input in Yes:
-                print("you lit the stick!")
-                inventory["Stick"] -= 1
-                print(inventory)
-                break
+                PlayerInventory()
+                Event = 2
+            elif usr_input in No:
+                Event = 2
         else:
             print("you have nothing on your person to make a torch")
-            break
+            Event = 2
 
-
-
-
+    if Event == 2:
+        pass
