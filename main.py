7880cca8bd9f2f95824dd39d58bab14d17c9faa6
Mahmoud Mahmoud <1906425@brunel.ac.uk>
2021-01-08 14:07:16 +0000

    I added in a function called "OptionsConfirm" that is called upon every time the player is asked to input the A key

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ hunger: 0 #if it reaches 10 the health begins to reduce gradually
 Yes = ["y", "Y", "yes", "Yes"]
 No = ["n", "N", "no", "No"]
 Confirm = ["a", "A"]
+LMR = ["Left", "Middle", "Right"]
 
 valueChange = 0
 
@@ -19,6 +20,18 @@ time.sleep(2)
 print("..")
 time.sleep(2)
 print("...")
+
+
+
+def OptionsConfirm(): #this function is called upon everytime the player is asked to input the A button to continue dialouge
+    global usr_input
+    while True:
+        usr_input = input("Press A")
+        if usr_input in Confirm:
+            break
+        else:
+            continue
+
 
 
 
@@ -34,7 +47,7 @@ def PlayerInventory(): #Adds things the player picks up during the game into the
                        #to the "events" the player is in.
     if Event == 0:
         inventory["Sticks"] = 5
-        print("The sticks were added to the inventory")
+        print("The bunch of sticks were added to the inventory")
         print(inventory)
 
     elif Event == 1:
@@ -59,42 +72,56 @@ def PlayerInventory(): #Adds things the player picks up during the game into the
 
 while True: #This is where all the events of the game play out
     if Event == 0:
-        usr_input = str(input("Press A to wake up"))
-        if usr_input in Confirm:
-            print(" 'Nghh...' ")
-            time.sleep(2)
-            print(" 'My head... where I am..?' ")
-            time.sleep(2)
-            print("""I took in my surroundings as my consciousness slowly reformed.
-            It seems that I am in a dark room, there is no window so I can't tell
-            what the time is. The room itself is mostly dark. Looking down  I see
-            a bunch of sticks lying around, which I picked up""")
-            usr_input = input("Press A")
-            if usr_input in Confirm:
-                PlayerInventory()
-                Event = 1
-            else:
-                continue
-        else:
-            continue #if the user inputs anything that is not in confirm the statement will repeat
+        OptionsConfirm()
+        print(" 'Nghh...' ")
+        time.sleep(2)
+        print(" 'My head... where I am..?' ")
+        time.sleep(2)
+        print("""I took in my surroundings as my consciousness slowly reformed.
+                        It seems that I am in a dark room, there were no window so I couldn't tell
+                        what the time was. The room itself was mostly dark. Looking down  I saw
+                        a bunch of sticks lying around, which I picked up""")
 
+        OptionsConfirm()
+        PlayerInventory()
+        Event = 1
 
     if Event == 1:
         print("'Oh I think I see some fire there, maybe I can make a couple of torches?'")
-        usr_input = input("Press A")
-        if usr_input in Confirm:
-            PlayerInventory()
-            Event = 2
+        OptionsConfirm()
+        PlayerInventory()
+        Event = 2
+
+    if Event == 2:
+        print("I used one of the torches to look around my surrondings again, until I found the exit of this room")
+        OptionsConfirm()
+        print("'Yes! A way out!'")
+        print("I then left the room without looking back. Outside was a corridor. In front were 3 doors ")
+        OptionsConfirm()
+        print("'Hmmm, which door to take?'")
+        time.sleep(2)
+        usr_input = input("Left/Middle/Right? :")
+        if usr_input in LMR:
+            if usr_input == "Left":
+                Event = "Left Door"
+            elif usr_input == "Right":
+                Event = "Right Door"
+            elif usr_input == "Middle":
+                Event = "Middle Door"
         else:
             continue
 
-    if Event == 2:
-        print("After making some torches I looked around some more until I found the exit of this room")
-        usr_input = input("Press A")
-        if usr_input in Confirm:
-            print("'Yes! A way out!'")
-            break
-        else:
-            continue
+
+
+    elif Event == "Left Door":
+        print("I went through the left door")
+        break
+    elif Event == "Right Door":
+        print("I took the door on the right")
+        break
+    elif Event == "Middle Door":
+        print("'You can't wrong with the middle door' I thought to myself")
+        break
+
 
 
